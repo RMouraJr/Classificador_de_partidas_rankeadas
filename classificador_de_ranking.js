13624ac0cf1ec4388b39450e6a6c2452fc2c6de7
Ronaldo Rodrigues de Moura Júnior <ronaldormoura10@gmail.com>
2024-06-14 14:35:18 -0300

    Classificador de partidas Rankeadas atualizado

--- classificador_de_ranking.js
+++ classificador_de_ranking.js
@@ -7,15 +7,20 @@ let numWins = parseInt (prompt("Me fale o seu número de vitórias ? "));
 let numLoses = parseInt(prompt("Agora preciso do seu número  de derrotas ? "));
 
 let nvRankeado;
+
 let eloRankeado;
+
 numVictory();
+
 rankeadas();
+
+console.log ("Caro Herói você possui o de saldo de " + nvRankeado + " vitórias. " + eloRankeado );
 
 function numVictory() {
     
         nvRankeado = numWins - numLoses
 }
-console.log ("Caro Herói você possui o de saldo de " + nvRankeado + " vitórias. " + eloRankeado )
+
 
 function rankeadas() {
     if (nvRankeado < 10) {
